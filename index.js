45f72035eea5f6bd4b8e36863fed776b540ec228
kholland950 <kholland950@capital.edu>
2017-10-28 11:47:46 -0400

    added resume

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 //scroller functions
 $(document).ready(function() {
-  $('.scrollTo, .side-nav-item, .scroller').click(function() {
+  $('.scrollTo, .side-nav-item, .scroller').click(function(e) {
     var target = $(this).data('target');
     var offset = 30;
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     if (w < 760) {
       offset = 0;
+    }
+    var target = $(this).data('target');
+    if (target != 'header') {
+      window.history.pushState({}, $(this).data('target') ,'#/' + $(this).data('target'));
+    } else {
+      window.history.pushState({}, 'Home', '/');
     }
     $('html,body').animate(
       {
@@ -15,7 +21,16 @@ $(document).ready(function() {
         ease: "easeout"
       }
     );
+    e.preventDefault();
   });
+
+  var path = window.location.hash;
+  if (path) {
+    var target = path.substring(2)
+    if (target.length > 0) {
+      $('[data-target="' + target + '"]').click();
+    }
+  }
 
   //menu bar scroll styling
   $('header > nav').toggleClass('highlight', $(window).scrollTop() > 0);
@@ -34,7 +49,6 @@ $(document).ready(function() {
   //close nav
   $("header nav ul li a").click(function(e) {
     $("header nav").removeClass("open");
-    e.preventDefault();
   });
 
   //doc close nav
